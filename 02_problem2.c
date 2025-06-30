f2314aa306496ddbea51905fb9279b6e277e9ad5
karan <karansaini1159@gmail.com>
2025-06-30 09:53:55 +0530

    chapter-5

---
+++ 02_problem2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+float c2f(float c);
+
+float c2f(float c){
+    return ((9*c)/5) +  32;
+}
+
+int main(){
+    float c = 45;
+    printf("celsius to fahrenhite for %f is %f", c, c2f(c));
+    return 0;
+}
